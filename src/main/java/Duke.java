a5df6f40a945ec5c63e7cf800af2697072583f7e
Papangkorn-Pann <pannranger@gmail.com>
2026-08-24 21:17:00 +0800

    Level-1: echoing inputs

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         Banner.print();
         System.out.println("Hi, I am Ducke! Your quackbot!");
         System.out.println("Waddle you want to do?");
+        Scanner scanner = new Scanner(System.in);
+        String command = "";
+        while (!command.equals("bye")) {
+            command = scanner.nextLine();
+            System.out.println(command);
+        }
         System.out.println("Quack quack! (bye bye)");
     }
 }
