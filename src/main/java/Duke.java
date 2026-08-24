e6c3f85d17699f78bb8d22c852c504e548cdec95
Papangkorn-Pann <pannranger@gmail.com>
2026-08-24 21:33:40 +0800

    Level-2: Task List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,36 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
         Banner.print();
         System.out.println("Hi, I am Ducke! Your quackbot!");
-        System.out.println("Waddle you want to do?");
+        System.out.println("Waddle you want me to do?");
         Scanner scanner = new Scanner(System.in);
         String command = "";
+        ArrayList<String> tasks = new ArrayList<>();
+        //tasks.add(newTask);
+        //tasks.size();
         while (!command.equals("bye")) {
             command = scanner.nextLine();
-            System.out.println(command);
+
+            if (command.equals("list")) {
+                if (!tasks.isEmpty()) {
+                    int counter = 1;
+                    for (String task : tasks) {
+                        System.out.print(counter);
+                        System.out.print(". ");
+                        System.out.println(task);
+                        counter++;
+                    }
+                } else {
+                    System.out.println("No tasks. Life is ponderful");
+                }
+            } else {
+                tasks.add(command);
+                System.out.print("Added: ");
+                System.out.println(command);
+            }
         }
         System.out.println("Quack quack! (bye bye)");
     }
