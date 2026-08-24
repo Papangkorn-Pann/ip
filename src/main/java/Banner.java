69fd88e195bb3eedc36b96dda52a5019797851dd
Papangkorn-Pann <pannranger@gmail.com>
2026-08-24 20:59:24 +0800

    Level-0: greet and exit. Also moved banner to separate file

---
+++ src/main/java/Banner.java
@@ -0,0 +1,10 @@
+public class Banner {
+    public static void print() {
+        String banner = " ____              _        \n"
+                + "|  _ \\ _   _  ____| | _____ \n"
+                + "| | | | | | |/ ___| |/ / _ \\\n"
+                + "| |_| | |_| | |___    <  __/\n"
+                + "|____/ \\__,_|\\____|__\\_\\___|\n";
+        System.out.println(banner);
+    }
+}
--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,8 @@
 public class Duke {
     public static void main(String[] args) {
-        String banner = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println(banner);
+        Banner.print();
+        System.out.println("Hi, I am Ducke! Your quackbot!");
+        System.out.println("Waddle you want to do?");
+        System.out.println("Quack quack! (bye bye)");
     }
 }
