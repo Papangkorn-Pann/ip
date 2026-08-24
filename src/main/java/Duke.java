2dd8c0f8b3b964ade7003ed462e7da648e532ca6
Papangkorn-Pann <pannranger@gmail.com>
2026-08-24 23:36:12 +0800

    Level-3: mark as done. Moved Task to a separate object as it now holds properties

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,26 +8,33 @@ public class Duke {
         System.out.println("Waddle you want me to do?");
         Scanner scanner = new Scanner(System.in);
         String command = "";
-        ArrayList<String> tasks = new ArrayList<>();
-        //tasks.add(newTask);
-        //tasks.size();
+        ArrayList<Task> tasks = new ArrayList<>();
+
         while (!command.equals("bye")) {
             command = scanner.nextLine();
 
             if (command.equals("list")) {
                 if (!tasks.isEmpty()) {
-                    int counter = 1;
-                    for (String task : tasks) {
-                        System.out.print(counter);
-                        System.out.print(". ");
-                        System.out.println(task);
-                        counter++;
+                    for (Task task : tasks) {
+                        task.printTask();
                     }
                 } else {
                     System.out.println("No tasks. Life is ponderful");
                 }
+            } else if (command.startsWith("mark ")) {
+                int index = Integer.parseInt(command.split(" ")[1]);
+                Task task = tasks.get(index - 1);
+                System.out.println("Quack! I've marked this task as done:");
+                task.markDone();
+                task.printTask();
+            } else if (command.startsWith("unmark ")) {
+                int index = Integer.parseInt(command.split(" ")[1]);
+                Task task = tasks.get(index - 1);
+                System.out.println("Quack! I've marked this task as not done yet");
+                task.unmarkDone();
+                task.printTask();
             } else {
-                tasks.add(command);
+                tasks.add(new Task(command));
                 System.out.print("Added: ");
                 System.out.println(command);
             }
---
+++ src/main/java/Task.java
@@ -0,0 +1,47 @@
+public class Task {
+
+    private static int count = 0;
+
+    private String name;
+    private Integer id;
+    private Boolean status; //true is done, false is not done
+
+    public Task(String name) {
+        count++;
+        this.name = name;
+        this.id = count;
+        this.status = false; //default task completion to false
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public String getStatusS() {
+        if (this.status) {
+            return "[X]";
+        } else {
+            return "[ ]";
+        }
+    }
+
+    public void markDone() {
+        this.status = true;
+    }
+
+    public void unmarkDone() {
+        this.status = false;
+    }
+
+    public void printTask() {
+        System.out.print(this.id);
+        System.out.print(". ");
+        System.out.print(this.getStatusS());
+        System.out.print(" ");
+        System.out.println(this.name);
+    }
+}
