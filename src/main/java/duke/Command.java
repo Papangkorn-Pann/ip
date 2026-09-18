4b783bfae9bdd516f7ea882d1c0443c0b3c45df9
Papangkorn-Pann <e1524004@u.nus.edu>
2026-09-18 10:52:50 +0800

    Added 'help' function and keyword Help function displays all other functions that Ducke is capable of understanding.

--- src/main/java/duke/Command.java
+++ src/main/java/duke/Command.java
@@ -4,5 +4,5 @@ package duke;
  * List of valid commands for Ducke
  */
 public enum Command {
-    LIST, MARK, UNMARK, TODO, DEADLINE, EVENT, DELETE, BYE, FIND
+    LIST, MARK, UNMARK, TODO, DEADLINE, EVENT, DELETE, BYE, FIND, HELP
 }
